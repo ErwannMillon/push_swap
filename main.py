311aad69f7850b24388ce297f6b7bed371d182f2
Glenn Millon <gmillon@c4r2p2.42nice.fr>
2022-05-08 16:56:19 +0200

    a

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import copy
+"""
+Starting approach:
+Check if optimal nums are those that are not in the LIS
+Start with relative sort: sorting by order, pathfinding number to be adjacent to its neighbor in sorted
+
+Principles:
+if a[0] > b[0], push to b
+if num_index = 1, decide between rotate and swap based on if a[-1] is higher or lower than a[0]
+
+
+Functions:
+move_above(x, y)
+move_below(y, x)
+
+
+"""
 def lis(list):
 	subsequences = []
 	for i in range(len(list)):
@@ -16,44 +32,99 @@ def lis(list):
 def s(stacks):
 	for stack in stacks:
 		if stack:
+			print("s")
 			stack.insert(1, stack.pop(0))
 def p(src, dest):
 	if src:
+		print("p")
 		dest.insert(0, src.pop(0))
 def rr(stacks):
 	for stack in stacks:
 		if stack:
+			print("rr")
 			stack.insert(0, stack.pop(-1))
 def r(stacks):
 	for stack in stacks:
 		if stack:
-			stack.insert(-1, stack.pop(0))
+			print("r")
+			stack.append(stack.pop(0))
 
-def sort_index(num, a, b, sortarr):
+def move_to_top(num, a, b, sortedarr):
 	num_index = a.index(num)
-	distance_to_top = num_index
+	num_after_insert_target = sortedarr[sortedarr.index(num) + 1]
+	index_after_insert_target = a.index(sortedarr[sortedarr.index(num) + 1])
+	num_before_insert_target = sortedarr[sortedarr.index(num) - 1]
+	index_before_insert_target = a.index(sortedarr[sortedarr.index(num) - 1])
 	distance_to_bottom = len(a) - num_index - 1
-	# distance_to_target = 
+	distance_to_top = num_index
+	path = []
+	if distance_to_bottom + 1 < distance_to_top:
+		print(f"{num} going to bottom")
+		for i in range(distance_to_bottom + 1):
+			if a.index(num) == 1 and a[0] < a[-1]:
+				path.append("sa")
+				s([a])
+			else:
+				path.append("rra")
+				rr([a])
+				print(a)
+	else:
+		print(f'{num} going to top')
+		for i in range(distance_to_top):
+			path.append("ra")
+			r([a])
+			print(a)
+	print(f'path: {path}')
+	return (path)
+
+def	path_find(num, a, b, sortedarr):
+	num_index = a.index(num)
+	num_after_insert_target = sortedarr[sortedarr.index(num) + 1]
+	index_after_insert_target = a.index(sortedarr[sortedarr.index(num) + 1])
+	num_before_insert_target = sortedarr[sortedarr.index(num) - 1]
+	index_before_insert_target = a.index(sortedarr[sortedarr.index(num) - 1])
+	distance_to_bottom = len(a) - num_index - 1
+	distance_to_top = num_index
+	path = []
+	# if num_index == index_after_insert_target or num_index == index_before_insert_target:
+	if 1:
+		path.extend(move_to_top(num, a, b, sortedarr))
+		print(path)
+		print(a)
+		 
+
+
+def sort_index(num, a, b, sortedarr):
+	num_index = a.index(num)
+	num_after_insert_target = sortedarr[sortedarr.index(num) + 1]
+	distance_to_bottom = len(a) - num_index - 1
+	distance_to_top = num_index
+	
+	# if a[a.index(num_after_insert_target)] < num and num != max(a):
+	# 	sort_index(num_after_insert_target, a, b, sortedarr)
+	# else:
+		# path_find(num, a, b, sortedarr)
+	path_find(num, a, b, sortedarr)
 
 
 def	main(a):
 	sortedarr = sorted(a)
 	b = []
-	for num in a:
-		sort_index(num, a, b);
-	
+	# for num in a:
+		# sort_index(num, a, b);
+	sort_index(a[1], a, b, sortedarr)
 
-b = [3, 2 , 1]
+test = [3, 2 , 1]
 arr = [10, 0, 1, 22, 9, 33, 21, 50, 41, 60]
-# print(lis(arr))
-print(arr)
-s([arr])
-print(arr)
-rr([arr])
-print(arr)
-p(arr, b)
-print("arr: ", arr)
-print ("b: ", b)
-r([arr])
-print(arr)
+# print(arr)
+# s([arr])
+# print(arr)
+# rr([arr])
+# print(arr)
+# p(arr, b)
+# print("arr: ", arr)
+# print ("b: ", b)
+# r([arr])
+# print(arr)
 
+main(arr)
