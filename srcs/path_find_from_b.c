277d1cd7ebc320a5d4523bc8106642e3ccaebc52
Erwann <erwannmillon@gmail.com>
2022-05-21 13:19:35 +0200

    changes readu to merge

--- srcs/path_find_from_b.c
+++ srcs/path_find_from_b.c
@@ -6,13 +6,25 @@
 /*   By: gmillon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 23:39:45 by gmillon           #+#    #+#             */
-/*   Updated: 2022/05/20 23:45:34 by gmillon          ###   ########.fr       */
+/*   Updated: 2022/05/21 10:34:04 by gmillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+t_arr	reinsert_before_target(t_node *num, t_node *a, t_node *b, t_arr sortedarr)
+{
+	const int	num_dist_top = ft_get_list_index(num->num, b);
+	const int	num_dist_bottom = dist_to_bottom(num->num, b);
+	const int	target_index = ft_get_list_index(num->next_in_sorted, a);
 
+	if (num_dist_top < num_dist_bottom + 1)
+	{
+		if (dist_to_top(num->next_in_sorted, a) - num_dist_top
+			> dist_to_bottom(num->next_in_sorted, a) + 1)
+	}
+
+}
 
 t_arr	path_find_from_b(t_node *num, t_node *a, t_node *b, t_arr sortedarr)
 {
@@ -23,6 +35,6 @@ t_arr	path_find_from_b(t_node *num, t_node *a, t_node *b, t_arr sortedarr)
 
 	if (ft_get_list_index(num->next_in_sorted, a) != -1)
 	{
-		path = reinsert_before_target(num, a, b, sortedarr)
+		path = reinsert_before_target(num, a, b, sortedarr);
 	}
 }
